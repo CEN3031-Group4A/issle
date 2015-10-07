5f0f48a3f1ee2712774923c1787c9af966b3b614
Raz Aloni <raztheraz@gmail.com>
2015-10-07 10:57:47 -0400

    Fixed stylings

--- modules/core/client/config/core.client.menus.js
+++ modules/core/client/config/core.client.menus.js
@@ -5,23 +5,23 @@
 
 angular.module('core').run(['Menus',
     function (Menus) {
-        Menus.addMenuItem('topbar', {
-            title: 'Standards',
-            state: 'standards',
-            isPublic: true
-        });
+        // Menus.addMenuItem('topbar', {
+        //     title: 'Standards',
+        //     state: 'standards',
+        //     isPublic: true
+        // });
 
-        Menus.addMenuItem('topbar', {
-            title: 'Search for Projects',
-            state: 'projectSearch',
-            isPublic: true
-        });
+        // Menus.addMenuItem('topbar', {
+        //     title: 'Search for Projects',
+        //     state: 'projectSearch',
+        //     isPublic: true
+        // });
 
-        Menus.addMenuItem('topbar', {
-            title: 'Support',
-            state: 'support',
-            isPublic: true
-        });
+        // Menus.addMenuItem('topbar', {
+        //     title: 'Support',
+        //     state: 'support',
+        //     isPublic: true
+        // });
     }
 ]);
 
