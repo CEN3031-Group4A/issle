394007871fc06204e721e081a077d5ec9b586609
Raz Aloni <raztheraz@gmail.com>
2015-10-12 13:33:34 -0400

    Linked Projects to FAQs and fixed up links

--- modules/support/client/config/support.client.routes.js
+++ modules/support/client/config/support.client.routes.js
@@ -5,11 +5,16 @@ angular.module('supports').config(['$stateProvider',
 	function($stateProvider) {
 		// Supports state routing
 		$stateProvider.
-		state('faq', {
+		state('support', {
+			abstract: true,
+			url: '/support',
+			template: '<ui-view/>'
+		}).
+		state('support.faq', {
 			url: '/FAQ',
 			templateUrl: 'modules/support/views/faq.client.view.html'
 		}).
-        state('about', {
+        state('support.about', {
             url: '/About',
             templateUrl: 'modules/support/views/about.client.view.html'
         });
