5dc8b7558378b2240b92b2e02782aa13c4bc4d04
Raz Aloni <raztheraz@gmail.com>
2015-10-22 01:03:35 -0400

    Account Page lists projects belonging to user

--- modules/projects/server/controllers/projects.server.controller.js
+++ modules/projects/server/controllers/projects.server.controller.js
@@ -73,7 +73,7 @@ exports.delete = function(req, res) {
 /**
  * List of Projects
  */
-exports.list = function(req, res) { Project.find().sort('-created').populate('user', 'displayName').exec(function(err, projects) {
+exports.list = function(req, res) { Project.find().where('user').equals(req.query['userID']).sort('-created').populate('user', 'displayName').exec(function(err, projects) {
 		if (err) {
 			return res.status(400).send({
 				message: errorHandler.getErrorMessage(err)
--- modules/users/client/controllers/settings/settings.client.controller.js
+++ modules/users/client/controllers/settings/settings.client.controller.js
@@ -1,7 +1,15 @@
 'use strict';
 
-angular.module('users').controller('SettingsController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('users').controller('SettingsController', ['$scope', 'Authentication','Projects',
+  function ($scope, Authentication, Projects){
     $scope.user = Authentication.user;
+
+    $scope.getUserProjects = function(){
+      Projects.query(
+        {'userID' : $scope.user._id},
+        function(projects) {
+          $scope.userProjects = projects;
+        }
+    );}
   }
 ]);
