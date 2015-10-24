25c8664243e9c4fd4d8ee817edac35dab3d5f113
woodmark888 <woodmark@ufl.edu>
2015-10-24 03:47:08 -0400

    Example page outline

--- modules/support/client/config/support.client.routes.js
+++ modules/support/client/config/support.client.routes.js
@@ -21,18 +21,30 @@ angular.module('supports').config(['$stateProvider',
         state('support.about', {
             url: '/About',
             templateUrl: 'modules/support/views/about.client.view.html'
-        });
+        }).
         state('support.example1', {
-            url: '/About',
+            url: '/example1',
             templateUrl: 'modules/support/views/example1.client.view.html'
-        });
+        }).
         state('support.example2', {
-            url: '/About',
+            url: '/example2',
             templateUrl: 'modules/support/views/example2.client.view.html'
-        });
+        }).
         state('support.example3', {
-            url: '/About',
+            url: '/example3',
             templateUrl: 'modules/support/views/example3.client.view.html'
+        }).
+        state('support.example4', {
+            url: '/example4',
+            templateUrl: 'modules/support/views/example4.client.view.html'
+        }).
+        state('support.example5', {
+            url: '/example5',
+            templateUrl: 'modules/support/views/example5.client.view.html'
+        }).
+        state('support.example6', {
+            url: '/example6',
+            templateUrl: 'modules/support/views/example6.client.view.html'
         });
 	}
 ]);
