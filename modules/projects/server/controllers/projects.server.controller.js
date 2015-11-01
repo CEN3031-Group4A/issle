f89628f0a51f33d219dc0ba732ef676ffba762d8
Raz Aloni <raztheraz@gmail.com>
2015-11-01 12:52:19 -0500

    Prettified Project.find() function

--- modules/projects/server/controllers/projects.server.controller.js
+++ modules/projects/server/controllers/projects.server.controller.js
@@ -74,11 +74,13 @@ exports.delete = function(req, res) {
  * List of Projects
  */
 exports.list = function(req, res) {
-	console.log('projectsByUser ' + req.query.userId);
-
 	if (req.query.userId)
 	{
-		Project.find().where('user').equals(req.query.userId).sort('-created').populate('user', 'displayName').exec(function(err, projects) {
+		Project.find()
+			.where('user').equals(req.query.userId)
+			.sort('-created')
+			.populate('user', 'displayName')
+			.exec(function(err, projects) {
 			if (err) {
 				return res.status(400).send({
 					message: errorHandler.getErrorMessage(err)
