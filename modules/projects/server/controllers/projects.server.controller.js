880c1299c265d53e106aaa984bfa00aee46d280c
Salimchaouqi <salimc@ufl.edu>
2015-11-01 16:29:31 -0500

    Implemented everything possible to do currently until yayati finishes with the schema on the creation in project search .

--- modules/projects/server/controllers/projects.server.controller.js
+++ modules/projects/server/controllers/projects.server.controller.js
@@ -3,114 +3,161 @@
 /**
  * Module dependencies.
  */
-var _ = require('lodash'),
-	path = require('path'),
-	mongoose = require('mongoose'),
-	Project = mongoose.model('Project'),
-	errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
+ var _ = require('lodash'),
+ path = require('path'),
+ mongoose = require('mongoose'),
+ Project = mongoose.model('Project'),
+ errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
 
 /**
  * Create a Project
  */
-exports.create = function(req, res) {
-	var project = new Project(req.body);
-	project.user = req.user;
+ exports.create = function(req, res) {
+ 	var project = new Project(req.body);
+ 	project.user = req.user;
 
-	project.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(project);
-		}
-	});
-};
+ 	project.save(function(err) {
+ 		if (err) {
+ 			return res.status(400).send({
+ 				message: errorHandler.getErrorMessage(err)
+ 			});
+ 		} else {
+ 			res.jsonp(project);
+ 		}
+ 	});
+ };
 
 /**
  * Show the current Project
  */
-exports.read = function(req, res) {
-	res.jsonp(req.project);
-};
+ exports.read = function(req, res) {
+ 	res.jsonp(req.project);
+ };
 
 /**
  * Update a Project
  */
-exports.update = function(req, res) {
-	var project = req.project ;
+ exports.update = function(req, res) {
+ 	var project = req.project ;
 
-	project = _.extend(project , req.body);
+ 	project = _.extend(project , req.body);
 
-	project.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(project);
-		}
-	});
-};
+ 	project.save(function(err) {
+ 		if (err) {
+ 			return res.status(400).send({
+ 				message: errorHandler.getErrorMessage(err)
+ 			});
+ 		} else {
+ 			res.jsonp(project);
+ 		}
+ 	});
+ };
 
 /**
  * Delete an Project
  */
-exports.delete = function(req, res) {
-	var project = req.project ;
+ exports.delete = function(req, res) {
+ 	var project = req.project ;
 
-	project.remove(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(project);
-		}
-	});
-};
+ 	project.remove(function(err) {
+ 		if (err) {
+ 			return res.status(400).send({
+ 				message: errorHandler.getErrorMessage(err)
+ 			});
+ 		} else {
+ 			res.jsonp(project);
+ 		}
+ 	});
+ };
 
 /**
  * List of Projects
  */
-exports.list = function(req, res) {
-	if (req.query.userId)
-	{
-		Project.find()
-			.where('user').equals(req.query.userId)
-			.sort('-created')
-			.populate('user', 'displayName')
-			.exec(function(err, projects) {
-			if (err) {
-				return res.status(400).send({
-					message: errorHandler.getErrorMessage(err)
-				});
-			} else {
-				res.jsonp(projects);
-			}
-		});
-	}
-	else
-	{
-		Project.find().sort('-created').populate('user', 'displayName').exec(function(err, projects) {
-			if (err) {
-				return res.status(400).send({
-					message: errorHandler.getErrorMessage(err)
-				});
-			} else {
-				res.jsonp(projects);
-			}
-		});
-	}
-};
+ exports.list = function(req, res) {
+ 	if (req.query.userId)
+ 	{
+ 		Project.find()
+ 		.where('user').equals(req.query.userId)
+ 		.sort('-created')
+ 		.populate('user', 'displayName')
+ 		.exec(function(err, projects) {
+ 			if (err) {
+ 				return res.status(400).send({
+ 					message: errorHandler.getErrorMessage(err)
+ 				});
+ 			} else {
+ 				res.jsonp(projects);
+ 			}
+ 		});
+ 	}
+ 	else
+ 	{
+ 		if(req.query.projectName) {
+ 			Project.find().
+ 			where('project.name').equals(req.query.projectName).
+ 			sort('-created').populate('user', 'displayName').
+ 			exec(function(err, projects) {
+ 				if (err) {
+ 					return res.status(400).send({
+ 						message: errorHandler.getErrorMessage(err)
+ 					});
+ 				} else {
+ 					res.jsonp(projects);
+ 				}
+ 			});
+ 		} else if(req.query.benchmark) {
+ 			Project.find().
+ 			where('benchmark').equals(req.query.benchmark).
+ 			sort('-created').populate('user', 'displayName').
+ 			exec(function(err, projects) {
+ 				if (err) {
+ 					return res.status(400).send({
+ 						message: errorHandler.getErrorMessage(err)
+ 					});
+ 				} else {
+ 					res.jsonp(projects);
+ 				}
+ 			});
+ 		} else if(req.query.subject) {
+ 			Project.find().
+ 			where('minGrade').gte(req.query.minGrade).
+ 			where('maxGrade').lte(req.query.maxGrade).
+ 			where('subject').equals(req.query.subject).
+ 			sort('-created').populate('user', 'displayName').
+ 			exec(function(err, projects) {
+ 				if (err) {
+ 					return res.status(400).send({
+ 						message: errorHandler.getErrorMessage(err)
+ 					});
+ 				} else {
+ 					res.jsonp(projects);
+ 				}
+ 			});
+ 		} else {
+ 			Project.find().
+ 			where('minGrade').gte(req.query.minGrade).
+ 			where('maxGrade').lte(req.query.maxGrade).
+ 			sort('-created').populate('user', 'displayName').
+ 			exec(function(err, projects) {
+ 				if (err) {
+ 					return res.status(400).send({
+ 						message: errorHandler.getErrorMessage(err)
+ 					});
+ 				} else {
+ 					res.jsonp(projects);
+ 				}
+ 			});
+ 		}
+ 	}
+ };
 
 /**
  * Project middleware
  */
-exports.projectByID = function(req, res, next, id) { Project.findById(id).populate('user', 'displayName').exec(function(err, project) {
-		if (err) return next(err);
-		if (! project) return next(new Error('Failed to load Project ' + id));
-		req.project = project ;
-		next();
-	});
+ exports.projectByID = function(req, res, next, id) { Project.findById(id).populate('user', 'displayName').exec(function(err, project) {
+ 	if (err) return next(err);
+ 	if (! project) return next(new Error('Failed to load Project ' + id));
+ 	req.project = project ;
+ 	next();
+ });
 };
