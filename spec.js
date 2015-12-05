dc3315038f6c6c9f8ef9a214d813b1f53bbf5e25
woodmark888 <woodmark@ufl.edu>
2015-12-05 06:08:08 -0500

    update protractor tests to include all parts of site besides search for project/standards

--- spec.js
+++ spec.js
@@ -1,159 +1,241 @@
 // spec.js
 describe('issle Demo App', function() {
-    // Home page
-   /* it('should be able to connect to the webapp', function() {
+    // Home page button test
+   it('Home page button test', function() {
         browser.get('http://localhost:3000/');
+        element(by.id('home')).click();
         expect(browser.getCurrentUrl()).toBe('http://localhost:3000/');
-    });
+   });
 
-    // See if in dev environment
-    it('should have a title', function() {
-        expect(browser.getTitle()).toEqual('issle - Development Environment');
-    });
+   // See if in dev environment test
+   it('See if in dev environment test', function() {
+       expect(browser.getTitle()).toEqual('issle - Development Environment');
+   });
 
-    // Search for projects
-    it('should be able to search for a project in the webapp', function() {
-        browser.get('http://localhost:3000/projects');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects');
-    });
+   // Search for projects
+   it('Search for projects', function() {
+       browser.get('http://localhost:3000/projects');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects');
+   });
 
-    // Create projects
-    it('should be able to create a project in the webapp', function() {
-        browser.get('http://localhost:3000/projects/create');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects/create');
-    });
+   // List standards
+   it('List standards', function() {
+       browser.get('http://localhost:3000/standards');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/standards');
+   });
 
-    // List standards
-    it('should be able to list standards in the webapp', function() {
-        browser.get('http://localhost:3000/standards');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/standards');
-    });
+   //Show example1 test
+   it('Show example1 test', function() {
+       browser.get('http://localhost:3000/support/examples');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/examples');
+       element(by.id('1')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/example1');
+   });
 
-    //Show examples
-    it('should be able to connect to see example projects in the webapp', function() {
-        browser.get('http://localhost:3000/support/examples');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/examples');
-    });
+   //Show example2 test
+   it('Show example2 test', function() {
+       browser.get('http://localhost:3000/support/examples');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/examples');
+       element(by.id('2')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/example2');
+   });
 
-    // FAQ
-    it('should be able to see FAQ for the webapp', function() {
-        browser.get('http://localhost:3000/support/FAQ');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/FAQ');
-    });
+   //Show example3 test
+   it('Show example3 test', function() {
+       browser.get('http://localhost:3000/support/examples');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/examples');
+       element(by.id('3')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/example3');
+   });
 
-    // About
-    it('should be able to see about page for the webapp', function() {
-        browser.get('http://localhost:3000/support/About');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/About');
-    });
+   //Show example4 test
+   it('Show example4 test', function() {
+       browser.get('http://localhost:3000/support/examples');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/examples');
+       element(by.id('4')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/example4');
+   });
 
-    // Create account
-    it('should be able to create account in the webapp', function() {
-        browser.get('http://localhost:3000/authentication/signup');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signup');
-    });
+   //Show example5 test
+   it('Show example5 test', function() {
+       browser.get('http://localhost:3000/support/examples');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/examples');
+       element(by.id('5')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/example5');
+   });
 
-    // Not Login
-    it('pretend to login but don\'t', function() {
-        browser.get('http://localhost:3000/authentication/signin');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
-    });
+   //Show example6 test
+   it('Show example6 test', function() {
+       browser.get('http://localhost:3000/support/examples');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/examples');
+       element(by.id('6')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/example6');
+   });
 
-    // Failure: My profile
-    it('shouldn\'t be able to traverse profile in the webapp cause you ain\'t signed in', function() {
-        browser.get('http://localhost:3000/settings/account');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
-    });
+   // FAQ page test
+   it('FAQ page test', function() {
+       browser.get('http://localhost:3000/support/FAQ');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/FAQ');
+       element(by.id('cc')).click();
+   });
 
-    // Failure: Edit profile
-    it('shouldn\'t be able to edit profile in the webapp cause you ain\'t signed in', function() {
-        browser.get('http://localhost:3000/settings/profile');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
-    });
+   // About page test
+   it('About page test', function() {
+       browser.get('http://localhost:3000/support/About');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/support/About');
+   });
 
-    // Failure: Change profile picture
-    it('shouldn\'t be able to change profile picture in the webapp cause you ain\'t signed in', function() {
-        browser.get('http://localhost:3000/settings/picture');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
-    });
+   // Fail login because no password test
+   it('Fail login because no password test', function() {
+       browser.get('http://localhost:3000/authentication/signin');
+       element(by.id('username')).clear();
+       element(by.id('username')).sendKeys('a');
+       element(by.id('password')).clear();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
+   });
 
-    // Failure: Change password
-    it('shouldn\'t be able to change password in the webapp cause you ain\'t signed in', function() {
-        browser.get('http://localhost:3000/settings/password');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
-    });*/
+   // Fail login because no username test
+   it('Fail login because no username test', function() {
+       browser.get('http://localhost:3000/authentication/signin');
+       element(by.id('username')).clear();
+       element(by.id('password')).clear();
+       element(by.id('password')).sendKeys('aaaaaaaa');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
+   });
 
-    // Failure: Signup
-    it('should be able to login to the webapp', function() {
-        browser.get('http://localhost:3000/authentication/signup');
-        element(by.id('username')).sendKeys('a');
-        element(by.id('password')).sendKeys('aaaaaaaa');
-        element(by.id('signup')).click();
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signup');
-    });
+   // Fail account access because not logged in test
+   it('Fail account access because not logged in test', function() {
+       browser.get('http://localhost:3000/settings/account');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
+   });
 
-    // Create project
-    it('should be able to login to the webapp', function() {
-        browser.get('http://localhost:3000/projects/create');
-        element(by.id('username')).sendKeys('a');
-        element(by.id('password')).sendKeys('aaaaaaaa');
-        element(by.id('login')).click();
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/');
-    });
+   // Fail profile access because not logged in test
+   it('Fail profile access because not logged in test', function() {
+       browser.get('http://localhost:3000/settings/profile');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
+   });
 
-    // Login
-    it('should be able to login to the webapp', function() {
-        browser.get('http://localhost:3000/authentication/signin');
-        element(by.id('username')).sendKeys('a');
-        element(by.id('password')).sendKeys('aaaaaaaa');
-        element(by.id('login')).click();
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/');
-    });
+   // Fail profile picture access because not logged in test
+   it('Fail profile picture access because not logged in test', function() {
+       browser.get('http://localhost:3000/settings/picture');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
+   });
 
-    // My profile
-    it('should be able to edit profile project in the webapp', function() {
-        browser.get('http://localhost:3000/settings/account');
-        element(by.id('search')).sendKeys('water');
-        element.all(by.repeater('userProjectItem in userProjects')).get(0).element(by.id('edit')).click();
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects/5654a24a2bb9c60e002ae187/edit');
-    });
+   // Fail profile password access because not logged in test
+   it('Fail profile password access because not logged in test', function() {
+       browser.get('http://localhost:3000/settings/password');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
+   });
 
-    // My profile
-    it('should be able to view profile project in the webapp then go to edit page', function() {
-        browser.get('http://localhost:3000/settings/account');
-        element(by.id('search')).sendKeys('water');
-        element.all(by.repeater('userProjectItem in userProjects')).get(0).element(by.id('link')).click();
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects/5654a24a2bb9c60e002ae187');
-        element(by.id('edit')).click();
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects/5654a24a2bb9c60e002ae187/edit');
-    });
+   // Fail signup because username in use test
+   it('Fail signup because username in use test', function() {
+       browser.get('http://localhost:3000/authentication/signup');
+       element(by.id('username')).clear();
+       element(by.id('username')).sendKeys('a');
+       element(by.id('password')).clear();
+       element(by.id('password')).sendKeys('aaaaaaaa');
+       element(by.id('email')).clear();
+       element(by.id('email')).sendKeys('email@email.com');
+       element(by.id('signup')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signup');
+   });
 
-    // Edit profile
-    /*it('should be able to edit profile in the webapp', function() {
-        browser.get('http://localhost:3000/settings/profile');
-        element(by.id('firstName')).sendKeys('us');
-        element(by.id('lastName')).sendKeys('ape');
-        element(by.id('school')).sendKeys('warts');
-        //element(by.id('email')).sendKeys('snape@hogwards.edu');
-        //element(by.id('username')).sendKeys('a');
-        element(by.id('submit')).click();
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/settings/profile');
-    });
+   // Fail signup because not an email test
+   it('Fail signup because not an email test', function() {
+       browser.get('http://localhost:3000/authentication/signup');
+       element(by.id('username')).clear();
+       element(by.id('username')).sendKeys('b');
+       element(by.id('password')).clear();
+       element(by.id('password')).sendKeys('bbbbbbbb');
+       element(by.id('email')).clear();
+       element(by.id('email')).sendKeys('notanemail');
+       element(by.id('signup')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signup');
+   });
 
-    // Change profile picture
-    it('shouldbe able to change profile picture in the webapp', function() {
-        browser.get('http://localhost:3000/settings/picture');
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/settings/picture');
-    });
+   // Successful login test
+   it('Successful login test', function() {
+       browser.get('http://localhost:3000/authentication/signin');
+       element(by.id('username')).clear();
+       element(by.id('username')).sendKeys('a');
+       element(by.id('password')).clear();
+       element(by.id('password')).sendKeys('aaaaaaaa');
+       element(by.id('login')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/');
+   });
 
-    // Change password
-    it('should be able to change password in the webapp', function() {
-        browser.get('http://localhost:3000/settings/password');
-        element(by.id('currentPassword')).sendKeys('aaaaaaaa');
-        element(by.id('newPassword')).sendKeys('aaaaaaaa');
-        element(by.id('verifyPassword')).sendKeys('aaaaaaaa');
-        element(by.id('changePassword')).click();
-        expect(browser.getCurrentUrl()).toBe('http://localhost:3000/settings/password');
-    });*/
+   // Create/Delete project test
+   it('Create/Delete project test', function() {
+       browser.get('http://localhost:3000/projects/create');
+       element(by.id('name')).sendKeys('testdat');
+       element(by.id('ss')).sendKeys('testdat');
+       element(by.id('#1')).click();
+       element(by.id('#2')).click();
+       element(by.id('#3')).click();
+       element(by.id('#4')).click();
+       element(by.id('#5')).click();
+       element(by.id('#6')).click();
+       element(by.id('#7')).click();
+       element(by.id('#8')).click();
+       element(by.id('#9')).click();
+       element(by.id('save')).click();
+       element(by.id('edit')).click();
+       element(by.id('sci')).sendKeys('testdat');
+       element(by.id('submit')).click();
+       element(by.id('delete')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects');
+   });
+
+   // My profile -> edit page
+   it('should be able to edit profile project in the webapp', function() {
+       browser.get('http://localhost:3000/settings/account');
+       element(by.id('search')).sendKeys('water');
+       element.all(by.repeater('userProjectItem in userProjects')).get(0).element(by.id('edit')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects/5654a24a2bb9c60e002ae187/edit');
+   });
+
+   // My profile -> view -> edit page
+   it('should be able to view profile project in the webapp then go to edit page', function() {
+       browser.get('http://localhost:3000/settings/account');
+       element(by.id('search')).sendKeys('water');
+       element.all(by.repeater('userProjectItem in userProjects')).get(0).element(by.id('link')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects/5654a24a2bb9c60e002ae187');
+       element(by.id('edit')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/projects/5654a24a2bb9c60e002ae187/edit');
+   });
+
+   // Edit profile settings test
+   it('Edit profile settings test', function() {
+       browser.get('http://localhost:3000/settings/profile');
+       element(by.id('firstName')).clear();
+       element(by.id('firstName')).sendKeys('Severus');
+       element(by.id('lastName')).clear();
+       element(by.id('lastName')).sendKeys('Snape');
+       element(by.id('school')).clear();
+       element(by.id('school')).sendKeys('Hogwarts');
+       element(by.id('email')).clear();
+       element(by.id('email')).sendKeys('snape@hogwards.edu');
+       element(by.id('username')).clear();
+       element(by.id('username')).sendKeys('a');
+       element(by.id('submit')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/settings/profile');
+   });
+
+   // Change profile picture test
+   it('Change profile picture test', function() {
+       browser.get('http://localhost:3000/settings/picture');
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/settings/picture');
+       element(by.id('pic')).click();
+   });
+
+   // Change profile password test
+   it('Change profile password test', function() {
+       browser.get('http://localhost:3000/settings/password');
+       element(by.id('currentPassword')).sendKeys('aaaaaaaa');
+       element(by.id('newPassword')).sendKeys('aaaaaaaa');
+       element(by.id('verifyPassword')).sendKeys('aaaaaaaa');
+       element(by.id('changePassword')).click();
+       expect(browser.getCurrentUrl()).toBe('http://localhost:3000/settings/password');
+   });
 
 });
